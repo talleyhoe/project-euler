1e94b8298be99d022f7a089800bd410d4b97b618
Ryan Talley <ryan.talley.mcgovern@gmail.com>
2023-01-06 16:55:09 -0500

    p7 code

---
+++ problems/id-0007/main.py
@@ -0,0 +1,67 @@
+# Problem 7
+# 10001st Prime
+
+import sys, os
+import math
+
+utils = "../../utils"
+path = os.path.realpath(utils)
+sys.path.append(path)
+
+from peuler_primes import sieve_erathosthenes
+
+def check_log(value):
+    if (value <= 0):
+        print(f"Invalid range for log(x). Need x > 0 (x: {value})",
+              file=sys.stderr)
+        sys.exit(1)
+    return 0
+
+def prime_density(ubound: int) -> float:
+    """
+    input: ubound - the upper bound of the number line to sieve through
+    output: density - estimated number of primes below ubound
+    """
+    check_log(ubound)
+    return ( ubound / math.log(ubound) )
+
+def prime_density_derivative(ubound: int) -> float:
+    check_log(ubound)
+    top = math.log(ubound) - 1
+    bottom = ( math.log(ubound) )**2
+    return (top / bottom)
+
+def eulers_method(func_value, func_derivative, 
+                  tolerance=20, guess=5, max_steps=200):
+    point = guess
+    value = func_value(point)
+    
+    steps: int = 0
+    while ( (abs(value) > tolerance) and (steps < max_steps) ): 
+        point -= ( func_value(point) / func_derivative(point) )
+        value = func_value(point)
+        steps += 1
+    return point
+    
+
+def main():
+    max_prime_cnt: int = 10_001
+    ubound_stride: int = ( 1 << 10)
+
+    root_func = lambda n: prime_density(n) - max_prime_cnt
+    ubound: int = int( eulers_method(root_func, prime_density_derivative) )
+    print(f"first prime pass. ubound: {ubound:,}")
+    primes: list[int] = sieve_erathosthenes(ubound)
+    prime_cnt: int = len(primes)
+    while ( prime_cnt < max_prime_cnt ):
+        primes = sieve_erathosthenes(ubound)
+        prime_cnt = len(primes)
+        ubound += ubound_stride
+
+    print(f"Prime {max_prime_cnt}: {primes[max_prime_cnt - 1]:,}")
+    return 0
+
+
+if __name__ == "__main__":
+    main()
+
--- utils/peuler_primes.py
+++ utils/peuler_primes.py
@@ -3,12 +3,30 @@
 import math
 import sys
 
-def sieve_erathosthenes(ubound: int) -> list[int]:
+def prime_density(ubound: int) -> float:
+    """
+    input: ubound - the upper bound of the number line to sieve through
+    output: density - estimated number of primes below ubound
+    """
+    return ( ubound / math.log(ubound) )
+
+def sieve_erathosthenes(ubound: int, primes: list[int] = None) -> list[int]:
     if (ubound < 1):
         print(f"Invalid range. Need upper bound > 1 (ubound:= {ubound})",
               file=sys.stderr)
         sys.exit(1)
     primes_bool: list[bool] = [True for i in range(2, (ubound + 1) )]
+    bool_len: int = len(primes_bool)
+
+    if (primes is not None):
+        largest_prime: int = primes[-1]
+        if (largest_prime > bool_len):
+            largest_prime = bool_len
+        for i in range(largest_prime):
+            if ( i in primes ):
+                continue
+            primes_bool[i] = False
+
     for p in range( 2, int( math.sqrt(ubound) ) ):
         p_idx = p - 2
         if ( primes_bool[p_idx] ):
